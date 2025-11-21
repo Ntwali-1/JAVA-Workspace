f4d1c8f6a4e5eab690c7a7f9fe114d42a862c03a
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-21 09:29:15 +0200

    feat: LambdaExpressions

--- src/lambda/CalculatorMain.java
+++ src/lambda/CalculatorMain.java
@@ -1,33 +1,33 @@
 package lambda;
 
-class Girl extends Thread {
-    @Override
-    public void run() {
-        for(int i = 0; i < 10; i++){
-            System.out.println("Girl");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-}
-
-class Boy extends Thread {
-    @Override
-    public void run() {
-        for(int i = 0; i < 10; i++){
-            System.out.println("Boy");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
+//class Girl extends Thread {
+//    @Override
+//    public void run() {
+//        for(int i = 0; i < 10; i++){
+//            System.out.println("Girl");
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
+//
+//}
+//
+//class Boy extends Thread {
+//    @Override
+//    public void run() {
+//        for(int i = 0; i < 10; i++){
+//            System.out.println("Boy");
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
+//}
 
 public class CalculatorMain {
     public static void main(String[] args) {
@@ -35,11 +35,32 @@ public class CalculatorMain {
         Calculator calc = (int a, int b) -> a + b;
         System.out.println(calc.calculate(1, 2));
 
-        //MultiThreading
-        Girl girl = new Girl();
-        Boy boy = new Boy();
-        girl.start();
-        boy.start();
+        Runnable girl = () -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Girl");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
 
+        Runnable boy = () -> {
+            for(int i = 0; i < 10; i++){
+                System.out.println("Boy");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        //MultiThreading
+        Thread t1 = new Thread(girl);
+        Thread t2 = new Thread(boy);
+        t1.start();
+        t2.start();
     }
 }
