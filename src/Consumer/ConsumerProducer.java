bfed21beb4a02b6cf6d96d006a32462273e8ac1c
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-27 16:05:27 +0200

    refactor: consumer

--- src/Consumer/ConsumerProducer.java
+++ src/Consumer/ConsumerProducer.java
@@ -1,4 +1,28 @@
 package Consumer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ConsumerProducer {
+    int capacity;
+    Queue<Integer> storage = new LinkedList<>();
+    public ConsumerProducer(int capacity){
+        this.capacity = capacity;
+    }
+    public synchronized void producer(int value) throws InterruptedException{
+        if(storage.size() == capacity){
+            wait();
+        }
+        storage.add(value);
+        System.out.println("Produced" + value);
+        notify();
+    }
+    public synchronized int consumer() throws InterruptedException{
+        if(storage.isEmpty()){
+            wait();
+        }
+        int value= storage.poll();
+        System.out.println("consumed"+value);
+        return value;
+    }
 }
