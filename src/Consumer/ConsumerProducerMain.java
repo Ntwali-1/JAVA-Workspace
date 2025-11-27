46f3dc2d111f6f31e43a87596c86c843ff0d4f10
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-27 16:05:56 +0200

    refactor: consumer

--- src/Consumer/ConsumerProducerMain.java
+++ src/Consumer/ConsumerProducerMain.java
@@ -1,4 +1,35 @@
 package Consumer;
 
 public class ConsumerProducerMain {
+    public static void main(String[] args){
+        ConsumerProducer c = new ConsumerProducer(10);
+        Thread t1 = new Thread(
+                () -> {
+                    for(int i = 1; i<50;i++){
+                        try{
+                            c.producer(i);
+                            Thread.sleep(100);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        };
+
+                    }
+                }
+        );
+        Thread t2 = new Thread(
+                () -> {
+                    for(int i = 1; i<50;i++){
+                        try{
+                            c.consumer();
+                            Thread.sleep(100);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        };
+
+                    }
+                }
+        );
+        t1.start();
+        t2.start();
+    }
 }
