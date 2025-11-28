a271c05bdcf0247ad71f257652c28684b3c83e5c
Ntwali-1 <ntwaliyanis@gmail.com>
2025-11-28 09:34:44 +0200

    update-regex

--- src/RegularExpressions/Regex.java
+++ src/RegularExpressions/Regex.java
@@ -5,7 +5,16 @@ import java.util.regex.Pattern;
 
 public class Regex {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("ca");
+        Pattern pattern = Pattern.compile(".ca");
         Matcher matcher = pattern.matcher("rca");
+
+        boolean result1 = matcher.matches();
+        System.out.println(result1);
+
+        boolean result2 = Pattern.compile("ca").matcher("rca").matches();
+        System.out.println(result2);
+
+        System.out.println(Pattern.matches("ca","rca"));
+        System.out.println(Pattern.matches("[amn]", "amn"));
     }
 }
